47e587c2206fc27035bfcd2ef19d62c35c52ec89
Sonoma <sameerhamid213@gmail.com>
2024-09-30 17:44:26 +0530

    learn dimensions api
    add responsiveness in NumberCntainer comp using dimensions api

--- 01-GuessNumber/components/ui/Title.js
+++ 01-GuessNumber/components/ui/Title.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet } from "react-native";
 
 function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
@@ -8,13 +8,15 @@ export default Title;
 
 const styles = StyleSheet.create({
   title: {
-    fontFamily: 'open-sans-bold',
+    fontFamily: "open-sans-bold",
     fontSize: 24,
     // fontWeight: 'bold',
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
     borderWidth: 2,
-    borderColor: 'white',
+    borderColor: "white",
     padding: 12,
+    maxWidth: "80%",
+    width: 300,
   },
 });
