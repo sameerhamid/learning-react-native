1b1585bfbaa2f9d10248ff14c0467dbd539ef96c
Sonoma <sameerhamid213@gmail.com>
2024-10-07 12:13:40 +0530

    get favorite meals data from context in FavoritesScreen

--- 02_Meals/screens/FavoritesScreen.js
+++ 02_Meals/screens/FavoritesScreen.js
@@ -1,7 +1,32 @@
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { useContext } from "react";
+import MealsList from "../components/MealsList/MealsList";
+import { FavroitesContext } from "../store/context/favorites-context";
+import { MEALS } from "../data/dummy-data";
 
 function FavoritesScreen() {
-  return <Text>The favorites screen!</Text>;
+  const { ids, removeFavorite } = useContext(FavroitesContext);
+  const favoriteMeals = MEALS.filter((meal) => ids.includes(meal.id));
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+    );
+  }
+  return <MealsList items={favoriteMeals} />;
 }
 
 export default FavoritesScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
