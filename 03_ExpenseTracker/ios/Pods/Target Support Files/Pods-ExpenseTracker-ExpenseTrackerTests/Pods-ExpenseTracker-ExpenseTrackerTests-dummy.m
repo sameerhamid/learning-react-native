bd346e05fed830c1127e272dd41a6391583325c0
Sonoma <sameerhamid213@gmail.com>
2024-10-08 12:11:37 +0530

    add gitignore

---
+++ 03_ExpenseTracker/ios/Pods/Target Support Files/Pods-ExpenseTracker-ExpenseTrackerTests/Pods-ExpenseTracker-ExpenseTrackerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ExpenseTracker_ExpenseTrackerTests : NSObject
+@end
+@implementation PodsDummy_Pods_ExpenseTracker_ExpenseTrackerTests
+@end
