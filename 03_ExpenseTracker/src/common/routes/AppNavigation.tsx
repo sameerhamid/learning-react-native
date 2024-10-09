2bb80e8a7b1e74de9d96fdbdfa8a3907c275f057
Sonoma <sameerhamid213@gmail.com>
2024-10-09 11:48:57 +0530

    change stack navigation to native stack

--- 03_ExpenseTracker/src/common/routes/AppNavigation.tsx
+++ 03_ExpenseTracker/src/common/routes/AppNavigation.tsx
@@ -1,22 +1,33 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 
 import { NavScreenTags } from "../constansts/NavScreenTags";
 import BottomTabsNavigatin from "./BottomTabsNavigatin";
 import ManageExpenses from "../../screens/ManageExpenses";
 import { LightTheme } from "../themes/lightTheme";
 import { navigationRef } from "../utils/navigatorUtils";
+import { GlobalStyles } from "../constansts/stylex";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 const AppNavigation = () => {
   const RootStack = (): React.ReactElement => {
     return (
-      <Stack.Navigator initialRouteName={NavScreenTags.BOTTOM_TAB_NAV}>
+      <Stack.Navigator
+        initialRouteName={NavScreenTags.BOTTOM_TAB_NAV}
+        screenOptions={() => ({
+          headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+          headerTintColor: "white",
+        })}
+      >
         <Stack.Screen
           name={NavScreenTags.BOTTOM_TAB_NAV}
           component={BottomTabsNavigatin}
-          options={{ headerShown: false }}
+          options={{
+            headerShown: false,
+            presentation: "modal",
+            animation: "slide_from_bottom",
+          }}
         />
         <Stack.Screen
           name={NavScreenTags.MANAGE_EXPENSES}
