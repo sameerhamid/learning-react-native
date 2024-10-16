2c53a1e7196ce540092e3e6fa889a514b644f341
Sonoma <sameerhamid213@gmail.com>
2024-10-16 17:10:07 +0530

    add asyncStorage to store token so that user don't have to login if token is present

--- 04__LearningAuthentication/src/common/store/auth-context.tsx
+++ 04__LearningAuthentication/src/common/store/auth-context.tsx
@@ -1,4 +1,6 @@
-import React, {createContext, useState, ReactNode} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, {createContext, useState, ReactNode, useEffect} from 'react';
+import {StorageKeys} from '../constants/enums';
 
 // Define the types for the context state
 interface AuthContextType {
@@ -6,6 +8,7 @@ interface AuthContextType {
   isAuthenticated: boolean;
   authenticate: (token: string) => void;
   logout: () => void;
+  loading: boolean;
 }
 
 // Create the context with default values
@@ -14,6 +17,7 @@ export const AuthContext = createContext<AuthContextType>({
   isAuthenticated: false,
   authenticate: () => {},
   logout: () => {},
+  loading: false,
 });
 
 // Define the type for the provider's props
@@ -24,20 +28,35 @@ interface AuthContextProviderProps {
 function AuthContextProvider({children}: AuthContextProviderProps) {
   const [token, setToken] = useState<string>('');
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [loading, setLoading] = useState(false);
 
   const authenticate = (token: string) => {
     setToken(token);
+    AsyncStorage.setItem(StorageKeys.TOKEN, token);
     setIsAuthenticated(true);
   };
 
   const logout = () => {
     setToken('');
+    AsyncStorage.removeItem(StorageKeys.TOKEN);
     setIsAuthenticated(false);
   };
 
+  useEffect(() => {
+    async function fetchToken() {
+      setLoading(true);
+      const storedToken = await AsyncStorage.getItem(StorageKeys.TOKEN);
+      if (storedToken) {
+        setToken(storedToken);
+        setIsAuthenticated(true);
+        setLoading(false);
+      }
+    }
+    fetchToken();
+  }, []);
   return (
     <AuthContext.Provider
-      value={{token, isAuthenticated, authenticate, logout}}>
+      value={{token, isAuthenticated, authenticate, logout, loading}}>
       {children}
     </AuthContext.Provider>
   );
