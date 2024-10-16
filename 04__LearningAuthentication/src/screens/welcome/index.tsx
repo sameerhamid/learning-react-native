b9b5bc12e04c17cbbf2d0f568dba6678e904405f
Sonoma <sameerhamid213@gmail.com>
2024-10-16 16:50:39 +0530

    get data from firebase

--- 04__LearningAuthentication/src/screens/welcome/index.tsx
+++ 04__LearningAuthentication/src/screens/welcome/index.tsx
@@ -1,10 +1,26 @@
+import axios from 'axios';
+import {useContext, useEffect, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {AuthContext} from '../../common/store/auth-context';
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+  const {token} = useContext(AuthContext);
+  useEffect(() => {
+    // i have set the rules for the api we have to pass token
+    axios
+      .get(
+        `https://react-native-authenticat-693d0-default-rtdb.firebaseio.com/message.json?auth=${token}`,
+      )
+      .then(res => {
+        setFetchedMessage(res.data);
+      });
+  }, []);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
