46f0f31fb5f49d56eec316ba05c19b787455cf5c
Sonoma <sameerhamid213@gmail.com>
2024-10-17 17:24:58 +0530

    add some styling to Placeform comp

--- 05_native-device-features/src/common/components/places/PlaceForm.tsx
+++ 05_native-device-features/src/common/components/places/PlaceForm.tsx
@@ -1,14 +1,44 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
+import React, {useState} from 'react';
+import {Colors} from '../../constants/Colors';
 
 const PlaceForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState<string>('');
+  const onTitleChageHandler = (text: string) => {
+    setEnteredTitle(enteredTitle);
+  };
   return (
-    <View>
-      <Text>PlaceForm</Text>
-    </View>
+    <ScrollView showsHorizontalScrollIndicator={false} style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          value={enteredTitle}
+          onChangeText={onTitleChageHandler}
+          style={styles.input}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
 export default PlaceForm;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    borderBottomColor: Colors.primary700,
+    borderWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
