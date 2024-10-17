4de2ed8a01028fab8ee29f73c1d92bf637b8259b
Sonoma <sameerhamid213@gmail.com>
2024-10-17 16:35:47 +0530

    add placesFrom comp

--- 05_native-device-features/src/screens/AddPlaces/index.tsx
+++ 05_native-device-features/src/screens/AddPlaces/index.tsx
@@ -1,14 +1,19 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import PlaceForm from '../../common/components/places/PlaceForm';
 
 const AddPlaces = () => {
   return (
-    <View>
-      <Text>AddPlaces</Text>
+    <View style={styles.container}>
+      <PlaceForm />
     </View>
   );
 };
 
 export default AddPlaces;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
